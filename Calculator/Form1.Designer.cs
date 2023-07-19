8dece831754dbea7a7d4f0d772f4a28a09b2aec1
ASHN101 <137661477+ASHN101@users.noreply.github.com>
2023-07-19 02:15:32 -0500

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.tbxInput1 = new System.Windows.Forms.NumericUpDown();
+            this.tbxInput2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.tbxInput1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbxInput2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblAnswer.Location = new System.Drawing.Point(267, 79);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(21, 24);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "=";
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(217, 110);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(43, 58);
+            this.btnMult.TabIndex = 3;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(282, 110);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(43, 58);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(331, 110);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(43, 58);
+            this.btnSub.TabIndex = 5;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(168, 110);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(43, 58);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // tbxInput1
+            // 
+            this.tbxInput1.Location = new System.Drawing.Point(31, 44);
+            this.tbxInput1.Name = "tbxInput1";
+            this.tbxInput1.Size = new System.Drawing.Size(242, 29);
+            this.tbxInput1.TabIndex = 7;
+            // 
+            // tbxInput2
+            // 
+            this.tbxInput2.Location = new System.Drawing.Point(279, 44);
+            this.tbxInput2.Name = "tbxInput2";
+            this.tbxInput2.Size = new System.Drawing.Size(242, 29);
+            this.tbxInput2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(540, 181);
+            this.Controls.Add(this.tbxInput2);
+            this.Controls.Add(this.tbxInput1);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.lblAnswer);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbxInput1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbxInput2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.NumericUpDown tbxInput1;
+        private System.Windows.Forms.NumericUpDown tbxInput2;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(tbxInput1.Text);
+            double num2 = double.Parse(tbxInput2.Text);
+            double answer = num1 / num2;
+            lblAnswer.Text = "= "+answer.ToString();
+
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(tbxInput1.Text);
+            double num2 = double.Parse(tbxInput2.Text);
+            double answer = num1 * num2;
+            lblAnswer.Text = "= " + answer.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(tbxInput1.Text);
+            double num2 = double.Parse(tbxInput2.Text);
+            double answer = num1 + num2;
+            lblAnswer.Text = "= " + answer.ToString();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(tbxInput1.Text);
+            double num2 = double.Parse(tbxInput2.Text);
+            double answer = num1 - num2;
+            lblAnswer.Text = "= " + answer.ToString();
+        }
+    }
+}
